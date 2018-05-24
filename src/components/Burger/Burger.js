b5d7e1193fc817a135f1c8813d794bc0a056a63f
Catherine Lin <catherine.lin.93@gmail.com>
2018-05-23 17:45:35 -0700

    build burger implemented

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import classes from './Burger.css';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+
+const burger = (props) => {
+    let transformedIngredients = Object.keys(props.ingredients) // return the keys of ingredients eg. salad, cheese
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])].map((_, i) => { // turn the number of ingredients in each category into an array to prevent errors since we are mapping through it
+                return <BurgerIngredient key={igKey + i} type={igKey} />
+            })
+        })
+        // reduce it to one single array
+        .reduce((arr,el) => {
+            return arr.concat(el);
+        }, []);
+        
+    console.log(transformedIngredients);
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please start adding ingredients!</p>
+    }
+
+    return (
+        <div className={classes.Burger}>
+            <BurgerIngredient type="bread-top" />
+                {transformedIngredients}
+            <BurgerIngredient type="bread-bottom" />
+        </div>
+    );
+}
+
+export default burger;
