f58940a8a3c5903d3ce5cf5b3a5b3f2dbb90fd35
Catherine Lin <catherine.lin.93@gmail.com>
2018-05-23 18:44:37 -0700

    implemented ingredients and price

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import classes from './BuildControls.css';
+import BuildControl from './BuildControl/BuildControl';
+
+const controls = [
+    { label: 'Salad', type: 'salad' },
+    { label: 'Bacon', type: 'bacon' },
+    { label: 'Cheese', type: 'cheese' },
+    { label: 'Meat', type: 'meat' },
+];
+
+const buildControls = (props) => (
+    <div className={classes.BuildControls}>
+        <p>Current Price: <strong>{props.price.toFixed(2)}</strong></p>
+        {controls.map(control => (
+            <BuildControl 
+                added={() => props.ingredientAdded(control.type)}
+                removed={() => props.ingredientsRemoved(control.type)}
+                disabled={props.disabled[control.type]}
+                key={control.label} 
+                label={control.label}/>
+        ))}
+    </div>
+);
+
+export default buildControls;
